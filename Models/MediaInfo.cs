f27ddfba41c0ca671322bac5f32bc72a3211979f
AlessP93 <alespiscino@libero.it>
2022-10-14 14:22:27 +0200

    aggiunto actor

--- Models/MediaInfo.cs
+++ Models/MediaInfo.cs
@@ -10,12 +10,9 @@ namespace csharp_boolflix.Models
         public int Id { get; set; }
         public string Year { get; set; }
         public bool IsNew { get; set; }
-
-        public List<Actor> Cast { get; set; }
-
-        public List<Genre> Generes { get; set; }//si relazione esterna
-
-        public List<Feature> Features { get; set; } //si relazione esterna
+        public List<Actor> Cast { get; set; } = new List<Actor>();
+        public List<Genre> Generes { get; set; } = new List<Genre>();
+        public List<Feature> Features { get; set; } = new List<Feature>(); 
 
         //1 a 1 su media content (film)
         public int? FilmId { get; set; }
